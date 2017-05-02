5c10422321de347c9bbf3436bb4943b829493adb
Vshodi <software@shodimeji.com>
2017-05-02 11:29:20 +0100

    Added Circuit Breaker pattern example on the planet api

---
+++ SolarSystem.Core/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace SolarSystem.Core
+{
+    public static class Constants
+    {
+        public static class API
+        {
+            public static int FAILURE_THRESHOLD = Convert.ToInt32(ConfigurationManager.AppSettings["API.FailureThreshold"]);
+            public static int OPEN_CIRCUIT_TIMEOUT = Convert.ToInt32(ConfigurationManager.AppSettings["API.OpenCircuitTimeout"]);
+        }
+    }
+}
--- SolarSystem.WebApi/Controllers/PlanetController.cs
+++ SolarSystem.WebApi/Controllers/PlanetController.cs
@@ -1,8 +1,8 @@
 ﻿using SolarSystem.Core;
-using SolarSystem.Data.Abstract;
 using SolarSystem.Models;
 using SolarSystem.Models.ViewModels;
 using SolarSystem.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -13,27 +13,34 @@ namespace SolarSystem.WebApi.Controllers
     {
         private readonly IPlanetRepository planetRepository;
         private readonly IProfileRepository profileRepository;
+        private static Helpers.CircuitBreaker.CircuitBreaker circuitBreaker;
 
         public PlanetController(IPlanetRepository planetRepository, IProfileRepository profileRepository)
         {
             this.planetRepository = planetRepository;
             this.profileRepository = profileRepository;
+
+            if (circuitBreaker == null)
+            {
+                circuitBreaker = new Helpers.CircuitBreaker.CircuitBreaker("planet_breaker", Constants.API.FAILURE_THRESHOLD, TimeSpan.FromSeconds(Constants.API.OPEN_CIRCUIT_TIMEOUT));
+            }
         }
 
         //GET api/planet
         public async Task<IEnumerable<Planet>> Get()
         {
-            return await planetRepository.GetPlanetsAsync();
+            return await circuitBreaker.ExecuteAsync(async () => { return await planetRepository.GetPlanetsAsync(); });
         }
 
         // GET api/planet/5
         public async Task<FullProfile> Get(int id)
         {
-            var planet = await planetRepository.GetPlanetAsync(id);
+            var planet = circuitBreaker.ExecuteAsync(async () => { return await planetRepository.GetPlanetAsync(id); });
+
             return new FullProfile
             {
-                SpaceBody = planet,
-                Profile = await profileRepository.GetProfileAsync(id, Helper.GetModelName(planet))
+                SpaceBody = await planet,
+                Profile = await circuitBreaker.ExecuteAsync(async () => { return await profileRepository.GetProfileAsync(id, Helper.GetModelName(planet)); })
             };
         }
 
